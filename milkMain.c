0fea1768126f5c1385675e8f39a4d54e258e05ec
AllMighty-57 <ecainepetty@gmail.com>
2026-03-11 19:42:57 -0700

    Expand main
    sorry for the reupload, forgot to actually save the file onto my drive

--- milkMain.c
+++ milkMain.c
@@ -7,15 +7,34 @@
 
 
 
-int main() 
+void onlyChance() 
 { 
+	printf"
 
 
+int main() 
+{							 // hp money
+  Player stats = {20, 100};
+  srand(time(NULL); //to randomly select routes, I'd advise you use seperate one to select the scenes in your route
+  int *health = &stats.hp;
+  int *money = &stats.money;
+	
+	printf("One day, you wake up to find that all your milk was gone. You groan as you head off to the store to get "
+          "more. While watching a random video your friend sent you, you walk into the street, not realizing the light\n"
+          "had just turned green. You turn your head just in time to see a semi honk its horn at you before everything "
+          "goes black.\n\nWhen you wake, you find yourself at home in your bed. What had happened? How did you get home? "
+          "How did you survive? Are you dead!?!\nWho are you kidding, you don't care. All you know is that you still "
+          "need to get milk. You throw yourself out of bed and begin your journey to the store to get a jug of milk.\n\n"); 
+	
+	printf("You start with 20 Hp and 100 dollars in your wallet as you leave your house./n/n"); 
+
+	int selection_list[4] = {1, 1, 1, 1}; 
+	int random_select;
 
 
+	while(quit != y) //Need to make it so that when it selects a route it removes the option, so no duplicate runs
+	{
+		random_select = rand()%4+1; //Also need to restore after end of loop
+		if(random_select == 1 && selction
 
-
-
-
-
-}
+	
