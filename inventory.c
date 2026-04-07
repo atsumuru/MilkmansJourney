592b59fb02099be2092419fd1e4fff2f029e4835
atsumuru <phillip8@my.erau.edu>
2026-04-07 13:05:19 -0700

    Update inventory.c

--- inventory.c
+++ inventory.c
@@ -1,17 +1,20 @@
+/*******************************************************
+*File:	  inventory.c
+*Author:  Payton Phillips
+*Purpose: This program allows the player to access the inventory.
+*Version: 1.0 April 1, 2026
+*         1.1 April 6, 2026
+*
+********************************************************/
+
 # include <stdio.h>
 # include <string.h>
+
+# include "creamCrop.h"
 
 # define MAX_SPECIAL 5
 # define MAX_SECRET 4
 # define SIZE 50
-
-// struct for player
-
-typedef struct
-{
-  int health;
-  int money;
-} Player;
 
 // struct for special inv
 
@@ -20,33 +23,19 @@ typedef struct
   char items[MAX_SPECIAL][SIZE];
   int count;
   
-  int emptyBottle;
-  int grilledCheese;
-  int umbrella;
-  int rubberChicken;
+  int EB;
+  int GC;
+  int UM;
+  int RC;
   
 } SpecialInventory;
-
-// struct for secret inv
-
-typedef struct
-{
-  char items[MAX_SECRET][SIZE];
-  int count;
-  
-  int butter;
-  int yogurt;
-  int iceCream;
-  int cheese;
-  
-} SecretInventory;
 
 // initialize player stats
 
 void initPlayer(Player * p)
 {
-  p -> health = 100;
-  p -> money = 0;
+  p -> hp = 100;
+  p -> money = 20;
 }
 
 // display player stats
@@ -54,7 +43,7 @@ void initPlayer(Player * p)
 void displayStats(Player * p)
 {
   printf("\n --- [PLAYER STATS] --- \n");
-  printf("Health: %d\n", p -> health);
+  printf("HP: %d\n", p -> hp);
   printf("Money: %d\n", p -> money);
 }
 
@@ -70,28 +59,10 @@ void initSpecial(SpecialInventory * inv)
     strcpy(inv -> items[i], ".....");
   }
   
-  inv -> emptyBottle = 0;
-  inv -> grilledCheese = 0;
-  inv -> umbrella = 0;
-  inv -> rubberChicken = 0;;
-}
-
-// initialize secret inv
-
-void initSecret(SecretInventory * inv)
-{
-  int i;
-
-  inv -> count = 0;
-  for (i = 0; i < MAX_SECRET; i++)
-  {
-    strcpy(inv -> items[i], ".....");
-  }
-  
-  inv -> butter = 0;
-  inv -> yogurt = 0;
-  inv -> iceCream = 0;
-  inv -> cheese = 0;
+  inv -> EB = 0;
+  inv -> GC = 0;
+  inv -> UM = 0;
+  inv -> RC = 0;
 }
 
 // add special items
@@ -103,65 +74,31 @@ void addSpecial(SpecialInventory * inv, char itemName[])
     strcpy(inv -> items [inv->count], itemName);
     inv -> count++;
     
-    printf("You found [SPECIAL ITEM]: %s\n", itemName);
+    printf("You found Special Item: %s\n", itemName);
     
     if (strcmp(itemName, "Empty Bottle") == 0)
     {
-      inv -> emptyBottle = 1;
+      inv -> EB = 1;
     }
       
     else if (strcmp(itemName, "Grilled Cheese") == 0)
     {
-      inv -> grilledCheese = 1;
+      inv -> GC = 1;
     }
       
     else if (strcmp(itemName, "Umbrella") == 0)
     {
-      inv -> umbrella = 1;
+      inv -> UM = 1;
     }
     else if (strcmp(itemName, "Rubber Chicken of Doom") == 0)
     {
-      inv -> rubberChicken = 1;
+      inv -> RC = 1;
     }
     
   }
   else
   {
-    printf("[SPECIAL ITEM] Inventory full!\n");
-  }
-}
-
-// add secret items
-
-void addSecret(SecretInventory * inv, char itemName[])
-{
-  if (inv -> count < MAX_SECRET)
-  {
-    strcpy(inv -> items [inv -> count], itemName);
-    inv -> count++;
-    printf("You found [SECRET ITEM]: %s\n", itemName);
-    
-    if (strcmp(itemName, "Butter") == 0)
-    {
-      inv -> butter = 1;
-    }
-      
-    else if (strcmp(itemName, "Yogurt") == 0)
-    {
-      inv -> yogurt = 1;
-    }
-      
-    else if (strcmp(itemName, "Ice Cream") == 0)
-    {
-      inv -> iceCream = 1;
-    }
-    else if (strcmp(itemName, "Cheese") == 0)
-    {
-      inv -> cheese = 1;
-  }
-  else
-  {
-    printf("[SECRET ITEM] Inventory full!\n");
+    printf("Inventory full!\n");
   }
 }
 
@@ -171,21 +108,8 @@ void displaySpecial(SpecialInventory * inv)
 {
   int i;
 
-  printf("\n --- [SPECIAL ITEMS] --- \n");
+  printf("\n --- INVENTORY --- \n");
   for (i = 0; i < MAX_SPECIAL; i++)
-  {
-    printf("Slot %d: %s\n", i + 1, inv -> items[i]);
-  }
-}
-
-// display secret inv
-
-void displaySecret(SecretInventory * inv)
-{
-  int i;
-
-  printf("\n --- [SECRET ITEMS] --- \n");
-  for (i = 0; i < MAX_SECRET; i++)
   {
     printf("Slot %d: %s\n", i + 1, inv -> items[i]);
   }
@@ -196,21 +120,18 @@ void displaySecret(SecretInventory * inv)
 int main () {
   Player player;
   SpecialInventory special;
-  SecretInventory secret;
   
   int choice = 0;
   
   initPlayer(&player);
   initSpecial(&special);
-  initSecret(&secret);
   
   while (choice != 4)
   {
-    printf("\n --- [MENU] --- \n");
+    printf("\n --- MENU --- \n");
     printf("1. View [PLAYER STATS]\n");
-    printf("2. View [SPECIAL ITEM] Inventory\n");
-    printf("3. View [SECRET ITEM] Inventory\n");
-    printf("4. Back to Game\n");
+    printf("2. View Inventory\n");
+    printf("3. Back to Game\n");
     printf("\n");
     
     printf("Choose option: ");
@@ -227,10 +148,6 @@ int main () {
         break;
       
       case 3:
-        displaySecret(&secret);
-        break;
-        
-      case 4:
         // back to game
         break;
         
