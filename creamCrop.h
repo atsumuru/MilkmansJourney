4d68094022a30534ea4d8ff12e523286b21d3b48
atsumuru <phillip8@my.erau.edu>
2026-04-08 16:09:14 -0700

    Add SI struct and route functions.

--- creamCrop.h
+++ creamCrop.h
@@ -1,18 +1,40 @@
 #ifndef HEADER_FILENAME
 #define HEADER_FILENAME
 
+// player struct
+
 typedef struct 
 { 
 	int hp; 
 	int money; 
 }Player;
 
+// secret inventory struct
+
+typedef struct
+{
+  char items[MAX_SPECIAL][SIZE];
+  int count;
+  
+  int EB;
+  int GC;
+  int UM;
+  int RC;
+  
+} SpecialInventory;
+
+// quit function
 
 char quit();
 
+// routes
 
+void grilledRoute (int *health, int *money, Inventory *inv);
+void umbrellaRoute (int *health, int *money, Inventory *inv);
 
+// other
 
-
+void routeReset (int list[], int size);
+int routeCheck (int list[], int size);
 
 #endif
