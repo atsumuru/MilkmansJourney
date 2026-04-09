5c336f2c7b022479eb5e396ba7fd815aa5ea5565
atsumuru <phillip8@my.erau.edu>
2026-04-09 13:26:03 -0700

    Add header file for umbrellaRoute.c

---
+++ umbrella.h
@@ -0,0 +1,24 @@
+#ifndef HEADER_FILENAME
+#define HEADER_FILENAME
+
+# define STEPS 4
+
+// colors
+
+# define RESET "\e[0m"
+# define YEL "\e[1;33m"
+# define BLU "\e[1;34m"
+# define RED "\e[1;31m"
+# define GRN "\e[0;32m"
+
+void sceneOne(int *hp, int *money);
+void sceneTwo(int *hp, int *money);
+void sceneThree(int *hp, int *money, Inventory *inv);
+void sceneFour(int *hp, int *money, Inventory *inv);
+
+void playRandomScene(int *hp, int *money, Inventory *inv);
+int umbrellaRoute(int *hp, int *money, Inventory *inv);
+
+char quit(void);
+
+#endif
