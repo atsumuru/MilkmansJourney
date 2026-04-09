78ca1b3a89b00e7ada4889e80bd62059f7a763b8
SirChickenBro <freddiewashington5555@gmail.com>
2026-04-09 16:49:10 -0700

    Add files via upload

---
+++ chickenRoute.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILENAME
+#define HEADER_FILENAME
+
+void clearInput();
+int inp(int x, int y, int z);
+void festival(int* m, INVENTORY* i);
+int bet(int q);
+void job(int* hp, int* money);
+void chickenRoute(int* health, int* money, Inventory *inv);
+
+#endif
