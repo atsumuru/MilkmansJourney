455f7ca7442b291c39d3102a198df5ea90ee3094
SirChickenBro <freddiewashington5555@gmail.com>
2026-04-10 16:30:43 -0700

    Add fprint for adventure log

--- chickenRoute.h
+++ chickenRoute.h
@@ -3,9 +3,9 @@
 
 void clearInput();
 int inp(int x, int y, int z);
-void festival(int* m, INVENTORY* i);
+void festival(int* m, INVENTORY* i, FILE* f);
 int bet(int q);
-void job(int* hp, int* money);
+void job(int* hp, int* money, FILE* f);
 void chickenRoute(int* health, int* money, Inventory *inv);
 
 #endif
