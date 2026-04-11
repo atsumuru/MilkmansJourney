a46966b632b89d135632417a0109d69057627b17
AllMighty-57 <ecainepetty@gmail.com>
2026-04-11 15:37:22 -0700

    Add files via upload

--- umbrellaRoute.h
+++ umbrellaRoute.h
@@ -1,13 +1,5 @@
-/*******************************************************
-*File:	   umbrellaRoute.h
-*Author:   Payton Phillips
-*Purpose:  This program is the header file for the umbrellaRoute. It contains all functions and defined variables.
-*Version:  1.0 April 9, 2026  
-*
-********************************************************/
-
-# ifndef HEADER_FILENAME
-# define HEADER_FILENAME
+#ifndef HEADER_FILENAME
+#define HEADER_FILENAME
 
 # define STEPS 4
 
@@ -27,4 +19,6 @@ void sceneFour(int *hp, int *money, Inventory *inv);
 void playRandomScene(int *hp, int *money, Inventory *inv);
 int umbrellaRoute(int *hp, int *money, Inventory *inv);
 
-# endif
+char quit(void);
+
+#endif
