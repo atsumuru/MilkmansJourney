84d15b3b14ccb13c940f41b260f464fb8bcbf59b
atsumuru <phillip8@my.erau.edu>
2026-04-11 20:07:04 -0700

    Update umbrellaRoute.h

--- umbrellaRoute.h
+++ umbrellaRoute.h
@@ -1,16 +1,6 @@
 #ifndef HEADER_FILENAME
 #define HEADER_FILENAME
 
-# define STEPS 4
-
-// colors
-
-# define RESET "\e[0m"
-# define YEL "\e[1;33m"
-# define BLU "\e[1;34m"
-# define RED "\e[1;31m"
-# define GRN "\e[0;32m"
-
 void sceneOne(int *hp, int *money, FILE *f);
 void sceneTwo(int *hp, FILE *f);
 void sceneThree(int *hp, int *money, Inventory *inv, FILE *f);
